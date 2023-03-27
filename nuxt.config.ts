11dea1b4c7568c8d86c023dd0365be7eb4e4c99b
Jesse Wierzbinski <contact@cpluspatch.com>
2023-03-26 14:50:00 -1000

    Add cache to fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/tailwindcss", "@nuxtjs/robots", "@nuxt/image-edge"],
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/robots",
+		"@nuxt/image-edge",
+		"@nuxtjs/google-fonts",
+	],
 	app: {
 		head: {
 			link: [
@@ -16,10 +21,19 @@ export default defineNuxtConfig({
 	},
 	nitro: {
 		compressPublicAssets: true,
+		routeRules: {
+			"/_nuxt/**": {
+				headers: {
+					"cache-control": `public,max-age=${
+						60 * 60 * 24 * 365
+					},s-maxage=${60 * 60 * 24 * 365}`,
+				},
+			},
+		},
 	},
 	image: {
 		ipx: {
 			maxAge: 60 * 60 * 24 * 365,
 		},
-	},
+	}
 });
