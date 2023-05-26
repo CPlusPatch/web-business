3aa82759332d963444a2cd3ecfb7602c556d1224
Jesse Wierzbinski <contact@cpluspatch.com>
2023-05-25 17:01:32 -1000

    Update website score section to fix width

--- components/landing/LighthouseScores.vue
+++ components/landing/LighthouseScores.vue
@@ -10,7 +10,7 @@ const stats = ref([
 
 <template>
 	<PrimaryContainer class="flex flex-col gap-y-10 items-start">
-		<div class="mx-auto max-w-2xl lg:mx-0">
+		<div class="mx-auto max-w-7xl lg:mx-0">
 			<h2
 				class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
 				Fast, powerful websites
