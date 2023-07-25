b45d2b054a244447c41b3e6a4dd6d3481c2e546f
Jesse Wierzbinski <contact@cpluspatch.com>
2023-07-25 01:28:00 -1000

    Add image picker to blocks

--- utils/utilities.ts
+++ utils/utilities.ts
@@ -1,4 +1,5 @@
 import { nanoid } from "nanoid";
+import { arrayBufferToWebP } from "webp-converter-browser";
 import { Setting } from "~/db/entities/Setting";
 
 export interface AnyObject {
@@ -27,3 +28,50 @@ export function generateIds(obj: AnyObject): AnyObject {
 
 export const getSettings = async () =>
 	(await useFetch<Setting>("/api/admin/settings")).data.value?.value ?? {};
+
+
+export const uploadFile = async (e: Event, token: string, setLoading: (loading: boolean) => void) => {
+	return new Promise(async (resolve, reject) => {
+		const target = e.target as HTMLInputElement;
+
+		if (!target.files?.length) return false;
+
+		setLoading(true);
+
+		// Dont convert WebP and SVG files to WebP automatically
+		const file: File =
+			target.files[0].type.includes("webp") ||
+			target.files[0].type.includes("svg")
+				? target.files[0]
+				: new File(
+						[
+							await arrayBufferToWebP(
+								await target.files[0].arrayBuffer()
+							),
+						],
+						target.files[0].name.split(".").slice(0, -1).join(".") +
+							".webp"
+				  );
+
+		const formData = new FormData();
+		formData.append("file", file);
+
+		fetch("/api/media/upload", {
+			method: "POST",
+			body: formData,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then(async res => {
+				if (res.ok) {
+					setLoading(false);
+					resolve(await res.text())
+				}
+			})
+			.catch(err => {
+				setLoading(false);
+				reject(err)
+			});
+	});
+};
